61afb03438beb5eeb5ea1b3279ca3848e1cba0f5
Ryo Takeuchi <develop@r09i.me>
2022-02-15 09:11:16 +0900

    :recycle: Refactor shell script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-FILE_PATH=$(cd "$(dirname "$0")" || exit; pwd)
+FILE_PATH=$(
+  cd "$(dirname "$0")" || exit
+  pwd
+)
 
 cd "$FILE_PATH/../" || exit
 docker-compose -p trampoline-chatbot -f docker-compose.yml up -d --build
